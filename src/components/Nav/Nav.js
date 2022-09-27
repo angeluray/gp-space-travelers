236aa26a575f3322476238f74c9f907d30d6c3cd
ghislainmitahi@gmail.com <ghislainmitahi@gmail.com>
2022-09-27 21:57:08 +0100

    add nav content

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,9 +1,40 @@
-import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { NavLink } from 'react-router-dom';
+import Logo from './world.png';
 
-const nav = () => (
-  <header>
-    <nav />
-  </header>
-);
+function Navigation() {
+  const activeLink = ({ isActive }) => ({
+    color: isActive ? '#0d6efd' : '',
+    paddingBottom: isActive ? '0.25rem' : '',
+    borderBottom: isActive ? '3px solid #0d6efd' : '',
+  });
 
-export default nav;
+  return (
+    <Navbar bg="light" expand="lg" className="border-bottom py-3">
+      <Container>
+        <Navbar.Brand href="#home" className="d-flex">
+          <img src={Logo} alt="Logo" className="logo" />
+          <h2 className="ps-3">Space-Travelers-Hub</h2>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link href="#rockets">
+              <NavLink style={activeLink} className="text-primary link" to="/">Rockets</NavLink>
+            </Nav.Link>
+            <Nav.Link href="#missions">
+              <NavLink style={activeLink} className="text-primary link" to="/mission">Missions</NavLink>
+            </Nav.Link>
+            <Nav.Link href="#myProfile">
+              <NavLink style={activeLink} className="text-primary link" to="/myProfile">My Profile</NavLink>
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Navigation;
