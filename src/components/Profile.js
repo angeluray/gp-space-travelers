57d0938c5ecf9e2eb5412680c2002a9e3712aeca
ghislainmitahi@gmail.com <ghislainmitahi@gmail.com>
2022-09-28 23:04:23 +0100

    add profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { ListGroup, Container } from 'react-bootstrap';
 
-const MyProfile = () => (
-  <p>Profile</p>
-);
+const MyProfile = () => {
+  const missions = useSelector((state) => state.mission);
+  const filteredMissions = missions.filter((mission) => mission.canceled === true);
+
+  return (
+    <Container className="d-flex justify-content-center mt-5">
+      <ListGroup className="w-25 me-5">
+        <h2>My Missions</h2>
+        {filteredMissions.length === 0 ? (
+          <p className="text-danger">Please would you like to join the mission with Favour?</p>
+        ) : (
+          filteredMissions.map((mission) => (
+            <ListGroup.Item key={mission.id}>{mission.name}</ListGroup.Item>
+          ))
+        )}
+      </ListGroup>
+    </Container>
+  );
+};
 
 export default MyProfile;
